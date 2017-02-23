0bf83759544baf90672eed4767b2f70cdff6259f
Vino4 <almensha@uoregon.edu>
2017-02-22 16:20:39 -0800

    Initial Publish

---
+++ Client/client.cpp
@@ -0,0 +1,72 @@
+/* 
+IBSSS Main Client 
+
+This client the following:
+	- Communication protocal
+	- Stream keys and stream access
+	- User settings and permissions
+Authors:
+Matt Almenshad | Andrew Gao | Jenny Horn 
+*/
+#include "client.h"
+#include "ibsss_op_codes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <bitset>
+#include <netdb.h>
+
+int main(int argc, char *argv[])
+{
+	if (IBSSS_DEBUG_MESSAGES && IBSSS_TRACE_OPCODES)
+		printOpCodes();
+	
+
+	int main_socket, server_port, out;
+	struct sockaddr_in server_address;
+	struct hostent * server;
+	char buffer[4096];
+	buffer[0] = 'H';
+	buffer[1] = 'e';
+	buffer[2] = 'l';
+	buffer[3] = 'l';
+	buffer[4] = 'o';
+	buffer[5] = '\0';
+	
+	if (argc >= 3){
+		server = gethostbyname(argv[1]);
+		server_port = atoi(argv[2]);
+	} else {
+		server = gethostbyname(IBSSS_DEFAULT_HOSTNAME);
+		server_port = IBSSS_DEFAULT_PORT;	
+	}
+	
+	main_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (main_socket < 0) 
+		ibsssError("could not open main socket");
+	bzero((char *) &server_address, sizeof(server_address));
+
+	bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(server_port);
+
+	if (connect(main_socket,(struct sockaddr *) &server_address,sizeof(server_address)) < 0) 
+		ibsssError("failed to establish a connection to the server");
+
+      if ((out = write(main_socket, &buffer, 6)) < 0)
+            ibsssError("failed to write");
+
+      if ((out = read(main_socket, buffer, 9)) < 0)
+            ibsssError("failed to read");
+
+	std::cout << buffer << std::endl;
+
+	close(main_socket);
+	
+	return 0; 
+}
---
+++ Specification/ibsss_op_codes.h
@@ -0,0 +1,56 @@
+/*
+IBSSS OP Codes
+This file defines the opcodes for the IBSSS communication protocal
+
+Authors:
+Matt Almenshad | Andrew Gao | Jenny Horn
+*/
+
+#ifndef _IBSSS_OP_CODES_HEADER
+#define _IBSSS_OP_CODES_HEADER
+
+
+const unsigned char IBSSS_OP_HELLO = 0 | 1 << 0;
+const unsigned char IBSSS_OP_CREATE_USER = 0 | 1 << 1;
+const unsigned char IBSSS_OP_LOGIN = 0 | 1 << 2;
+const unsigned char IBSSS_OP_LOGOUT = 0 | 1 << 2 | 1 << 0;
+const unsigned char IBSSS_OP_REQUEST_STREAM_LINKS = 0 | 1 << 3;
+const unsigned char IBSSS_OP_REQUEST_STREAM_KEY = 0 | 1 << 4;
+const unsigned char IBSSS_OP_CHANGE_PASSWORD = 0 | 1 << 5;
+const unsigned char IBSSS_OP_FORGOT_PASSWORD = 0 | 1 << 6;
+const unsigned char IBSSS_OP_FORGOT_USERNAME = 0 | 1 << 7;
+
+void printOpCodes(){
+	
+	std::cout 
+	<< "IBSSS_OP_HELLO :" 
+	<< std::bitset<8> (IBSSS_OP_HELLO) 
+	<< std::endl 
+	<< "IBSSS_OP_CREATE_USER:" 
+	<< std::bitset<8> (IBSSS_OP_CREATE_USER) 
+	<< std::endl 
+	<< "IBSSS_OP_LOGIN :" 
+	<< std::bitset<8> (IBSSS_OP_LOGIN) 
+	<< std::endl 
+	<< "IBSSS_OP_LOGOUT :" 
+	<< std::bitset<8> (IBSSS_OP_LOGOUT) 
+	<< std::endl 
+	<< "IBSSS_OP_REQUEST_STREAM_LINKS :" 
+	<< std::bitset<8> (IBSSS_OP_REQUEST_STREAM_LINKS) 
+	<< std::endl 
+	<< "IBSSS_OP_REQUEST_STREAM_KEY :" 
+	<< std::bitset<8> (IBSSS_OP_REQUEST_STREAM_KEY) 
+	<< std::endl 
+	<< "IBSSS_OP_CHANGE_PASSWORD :" 
+	<< std::bitset<8> (IBSSS_OP_CHANGE_PASSWORD) 
+	<< std::endl 
+	<< "IBSSS_OP_FORGOT_PASSWORD :" 
+	<< std::bitset<8> (IBSSS_OP_FORGOT_PASSWORD) 
+	<< std::endl 
+	<< "IBSSS_OP_FORGOT_USERNAME :" 
+	<< std::bitset<8> (IBSSS_OP_FORGOT_USERNAME) 
+	<< std::endl; 
+}
+
+#endif /*_IBSSS_OP_CODES_HEADER*/
+
