bb7646b5afa090db46c137b22190a5cb8e531655
Jenny Horn <jhorn3@uoregon.edu>
2017-03-03 19:17:01 -0800

    added initial GUI

---
+++ new_client/GUI/streamViewer/main.cpp
@@ -0,0 +1,11 @@
+#include "loginpage.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    LoginPage w;
+    w.show();
+//;ALKJSDAFLKSDJFA;LSDFOIAS;EOIA; LNSD;FISDKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKLLLLLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOZ
+    return a.exec();
+}
---
+++ new_client/GUI/streamViewer/streamview.h
@@ -0,0 +1,39 @@
+#ifndef STREAMVIEW_H
+#define STREAMVIEW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+
+class QActionGroup;
+class QButtonGroup;
+class QStringList;
+
+namespace Ui {
+class StreamView;
+}
+
+class StreamView : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit StreamView(QWidget *parent = 0);
+    ~StreamView();
+
+private slots:
+    void on_actionLog_out_triggered();
+
+    //void on_actionManage_Streams_triggered();
+    void showSection(QAction* a);
+    void backToStreamview();
+    //void on_back_clicked();
+
+private:
+    Ui::StreamView *ui;
+    QActionGroup* sections;
+    QButtonGroup* backs;
+    QStringList listy;
+    QGraphicsScene *scene;
+};
+
+#endif // STREAMVIEW_H
