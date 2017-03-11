099d6429482a6fde76dffb17b885b4f9b5bc12aa
Vino4 <almensha@uoregon.edu>
2017-03-10 17:55:20 -0800

    Cleaning up da mess

--- client/client.cc
+++ client/client.cc
@@ -1,83 +1,59 @@
 /* 
-IBSSS Main Client 
+IBSSS Main Server
 
-This client the following:
+This file runs the ibsss server
+The ibsss server manages the following:
+	- Users, streams, stream keys and access control
 	- Communication protocal
-	- Stream keys and stream access
-	- User settings and permissions
+
 Authors:
 Matt Almenshad | Andrew Gao | Jenny Horn 
 */
-#include "client.hh"
+
+#include "ibsss_server_connection_handler.hh"
 #include "ibsss_op_codes.hh"
+#include "ibsss_error.hh"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <iostream>
-#include <bitset>
-#include <netdb.h>
 
-int main(int argc, char *argv[])
-{
-	if (IBSSS_DEBUG_MESSAGES && IBSSS_TRACE_OPCODES)
-		printOpCodes();
+int main(int argc, char *argv[]){
+	Server_Connection_Handle connection;
+	if (connection.connect())
+		std::cout << "conNeKtid" << std::endl;
+	else
+		std::cout << "Fak" << std::endl;
 	
+	std::string username("test");
+	std::string email("lol@iOiOiO.fuck");
+	std::string password("KTNXBYE");
+	std::string new_password("lolololol");
 
-	int main_socket, server_port, out;
-	struct sockaddr_in server_address;
-	struct hostent * server;
-	char buffer[4096];
-	buffer[0] = 'H';
-	buffer[1] = 'e';
-	buffer[2] = 'l';
-	buffer[3] = 'l';
-	buffer[4] = 'o';
-	buffer[5] = '\0';
-	
-	if (argc >= 3){
-		server = gethostbyname(argv[1]);
-		server_port = atoi(argv[2]);
-	} else {
-		server = gethostbyname(IBSSS_DEFAULT_HOSTNAME);
-		server_port = IBSSS_DEFAULT_PORT;	
-	}
-	
-	main_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (main_socket < 0) 
-		ibsssError("could not open main socket");
-	bzero((char *) &server_address, sizeof(server_address));
+	if(connection.operationCreateUser(username, password, email))
+		std::cout << "Made It" << std::endl;
+	else
+		std::cout << "Fucked" << std::endl;
 
-	bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(server_port);
+	if(connection.operationLogin(username, password))
+		std::cout << "Killing It" << std::endl;
+	else
+		std::cout << "Fakedd" << std::endl;
 
-	if (connect(main_socket,(struct sockaddr *) &server_address,sizeof(server_address)) < 0) 
-		ibsssError("failed to establish a connection to the server");
+	if(connection.operationChangePassword(username, new_password, password))
+		std::cout << "chAAAAINJED" << std::endl;
+	else
+		std::cout << "no chnj :(" << std::endl;
 
-	std::string input;
+	if(connection.operationLogin(username, password))
+		std::cout << "old pass: yes" << std::endl;
+	else
+		std::cout << "old pass: nah" << std::endl;
 
-	for (;;){
-		std::cin >> input;	
-		unsigned int length = input.size();	
-		std::cout << "Sending Length: " << length << std::endl;
-		if ((out = write(main_socket, &length, 4)) < 0)
-      	      ibsssError("failed to write");
-		
-		std::cout << "Sending input: " << input << std::endl;
-		if ((out = write(main_socket, input.c_str(), length)) < 0)
-      	      ibsssError("failed to write");
-	
-	      if ((out = read(main_socket, buffer, 9)) < 0)
-	            ibsssError("failed to read");	
+	if(connection.operationLogin(username, new_password))
+		std::cout << "NEW PASS IN" << std::endl;
+	else
+		std::cout << "new pass sad :(" << std::endl;
 
-		std::cout << "Received: " <<  buffer << std::endl;
-	}
-
-	close(main_socket);
+	std::cout << "NA NA NA NANA" << std::endl;	
 	
 	return 0; 
 }
